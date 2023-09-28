17705bb7da7add339b1887e58a0b5053d1bf96c3
paalaman45 <kissel@sitesatscale.com>
2023-09-28 13:52:22 +0800

    Laravel Api: View, Create, Update and Delete.

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Api\V1\CompletedTaskController;
+use App\Http\Controllers\Api\V1\TaskController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +15,12 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+
+//V1 Api Collection
+Route::prefix('v1')->group(function (){
+    Route::apiResource('/tasks',TaskController::class);
+    Route::patch('/tasks/{task}/complete',CompletedTaskController::class);
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
