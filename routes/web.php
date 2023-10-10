8ad0b28d1062a0cf4b0ad4edb4e9174f28a62c7e
paalaman45 <kissel@sitesatscale.com>
2023-10-11 07:23:52 +0800

    Removed Homepage

--- routes/web.php
+++ routes/web.php
@@ -16,9 +16,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Route::prefix('auth')->group(function (){
     Route::post('/login',LoginController::class)->middleware('guest');
